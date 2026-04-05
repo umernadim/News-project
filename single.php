1aba3e76c320978ef0daa5fe9032f541e0c0f389
umernadim <itsumernadeem@gmail.com>
2026-04-05 18:47:06 +0500

    post displayed

--- single.php
+++ single.php
@@ -1,36 +1,66 @@
 <?php include 'header.php'; ?>
-    <div id="main-content">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                  <!-- post-container -->
-                    <div class="post-container">
-                        <div class="post-content single-post">
-                            <h3>Lorem ipsum dolor sit amet, consectetur</h3>
-                            <div class="post-information">
-                                <span>
-                                    <i class="fa fa-tags" aria-hidden="true"></i>
-                                    Html
-                                </span>
-                                <span>
-                                    <i class="fa fa-user" aria-hidden="true"></i>
-                                    <a href='author.php'>Admin</a>
-                                </span>
-                                <span>
-                                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                                    01 Nov, 2019
-                                </span>
-                            </div>
-                            <img class="single-feature-image" src="images/post_1.jpg" alt=""/>
-                            <p class="description">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                            </p>
-                        </div>
+<div id="main-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <!-- post-container -->
+                <div class="post-container">
+                    <div class="post-content single-post">
+                        <?php
+                        include 'admin/config.php';
+
+                        // URL se post id lena
+                        if (isset($_GET['id'])) {
+                            $post_id = $_GET['id'];
+
+                            $sql = "SELECT p.post_id, p.title, p.description, p.post_date, 
+                                           c.category_name, p.post_img, p.category, 
+                                           u.username, u.user_id
+                                    FROM post AS p
+                                    INNER JOIN category AS c ON p.category = c.category_id
+                                    INNER JOIN user AS u ON p.author = u.user_id
+                                    WHERE p.post_id = {$post_id}";
+
+                            $result = mysqli_query($connect, $sql);
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                        ?>
+                                    <h3><?php echo $row['title']; ?></h3>
+                                    <div class="post-information">
+                                        <span>
+                                            <i class="fa fa-tags" aria-hidden="true"></i>
+                                            <?php echo $row['category_name']; ?>
+                                        </span>
+                                        <span>
+                                            <i class="fa fa-user" aria-hidden="true"></i>
+                                            <a href='author.php?aid=<?php echo $row['user_id']; ?>'>
+                                                <?php echo $row['username']; ?>
+                                            </a>
+                                        </span>
+                                        <span>
+                                            <i class="fa fa-calendar" aria-hidden="true"></i>
+                                            <?php echo $row['post_date']; ?>
+                                        </span>
+                                    </div>
+                                    <img class="single-feature-image" src="admin/upload/<?php echo $row['post_img']; ?>" alt="" />
+                                    <p class="description">
+                                        <?php echo $row['description']; ?>
+                                    </p>
+                        <?php
+                                }
+                            } else {
+                                echo "<h2>No Post Found.</h2>";
+                            }
+                        } else {
+                            echo "<h2>Invalid Request.</h2>";
+                        }
+                        ?>
                     </div>
-                    <!-- /post-container -->
                 </div>
-                <?php include 'sidebar.php'; ?>
+                <!-- /post-container -->
             </div>
+            <?php include 'sidebar.php'; ?>
         </div>
     </div>
+</div>
 <?php include 'footer.php'; ?>
