22e6f1b38a761f5f0ac2cde72f83494c480710bb
umernadim <itsumernadeem@gmail.com>
2026-04-07 13:00:47 +0500

    category page updated

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,35 +14,56 @@
     <!-- Custom stlylesheet -->
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-<!-- HEADER -->
-<div id="header">
-    <!-- container -->
-    <div class="container">
-        <!-- row -->
-        <div class="row">
-            <!-- LOGO -->
-            <div class=" col-md-offset-4 col-md-4">
-                <a href="index.php" id="logo"><img src="images/news.jpg"></a>
-            </div>
-            <!-- /LOGO -->
-        </div>
-    </div>
-</div>
-<!-- /HEADER -->
-<!-- Menu Bar -->
-<div id="menu-bar">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <ul class='menu'>
-                    <li><a href='category.php'>Business</a></li>
-                    <li><a href='category.php'>Entertainment</a></li>
-                    <li><a href='category.php'>Sports</a></li>
-                    <li><a href='category.php'>Politics</a></li>
-                </ul>
+    <!-- HEADER -->
+    <div id="header">
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <!-- LOGO -->
+                <div class=" col-md-offset-4 col-md-4">
+                    <a href="index.php" id="logo"><img src="images/news.jpg"></a>
+                </div>
+                <!-- /LOGO -->
             </div>
         </div>
     </div>
-</div>
-<!-- /Menu Bar -->
+    <!-- /HEADER -->
+    <!-- Menu Bar -->
+    <div id="menu-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php
+                    include 'admin/config.php';
+                    if (isset($_GET['cid'])) {
+                        $cat_id = $_GET['cid'];
+                    }
+                    $sql = "SELECT * FROM category WHERE post > 0";
+                    $result = mysqli_query($connect, $sql);
+                    if (mysqli_num_rows($result) > 0) {
+                        $active = "";
+                    ?>
+                        <ul class='menu'>
+                            <li><a href="index.php">Home</a></li>
+                            <?php
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                if (isset($_GET['cid'])) {
+                                    if ($row['category_id'] == $cat_id) {
+                                        $active = "active";
+                                    } else {
+                                        $active = "";
+                                    }
+                                }
+
+                                echo "<li><a class='{$active}' href='category.php?cid={$row['category_id']}'>{$row['category_name']} </a></li>";
+                            } ?>
+                        </ul>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /Menu Bar -->
